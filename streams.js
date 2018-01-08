bf373de29deeb3f78ae2ac0719c2748a90986675
mast900 <magdalena.stenius@securitas.se>
2018-01-08 09:01:58 +0100

    Add encr (split2 and through)

--- streams.js
+++ streams.js
@@ -1,13 +1,34 @@
 // Require Node file system
-var fs = require('fs'); 
+const fs = require('fs')
+const split = require('split2')
+const through2 = require('through2')
 
-// By using the node file system you can read, create, update, delete and rename files. 
 let read = fs.createReadStream('./my.txt')
+let write = fs.createWriteStream('./copy.txt')
 
-read.on('data', (chunk) => {
-    console.log(`Recieved ${chunk.length} bytes of data.`)
+
+// By using the node file system you can read, create, update, delete and rename files.
+// This is a readable stream 
+read.on('data', (piece) => {
+    console.log(`Recieved ${piece.length} bytes of data.`)
 })
 
-let write = fs.createWriteStream('./copy.txt')
+// ...and this is a writeable stream
 read.pipe(write)
+
+// One can also transform streams (this one creates an encrypted file)
+read
+    .pipe(split())
+    .pipe(through2(function (piece, encryption, callback) {
+        for (var i = 0; i < piece.length; i++)
+            piece[i] = piece[i] + 3
+
+        this.push(piece)
+        
+        callback()
+    }))
+    .pipe(write)
+    .on('finish', function () {
+        console.log('A new encrypted file was created.')
+    })
 
